ec67cca201c0b274e35b3a990692b5ff101c0143
Илья Кабанов <iliakabanov1807@gmail.com>
2025-02-28 16:05:18 +0200

    mend

--- Application/Validators/AccessTriggerLogValidator.cs
+++ Application/Validators/AccessTriggerLogValidator.cs
@@ -11,7 +11,12 @@ namespace Application.Validators
         public AccessTriggerLogValidator()
         {
             RuleFor(x => x.AccessLogId)
-                .Must(x => x != Guid.Empty).WithMessage(x => InvalidProperty(nameof(x.AccessLogId)));
+                .Must(x => x != Guid.Empty)
+                .WithMessage(x => NotEmpty(nameof(x.AccessLogId)));
+
+            RuleFor(x => x.AccessLogId.ToString())
+                .Matches(@"^[{]?[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}[}]?$")
+                .WithMessage(x => InvalidProperty(nameof(x.AccessLogId)));
 
             RuleFor(x => x.TriggerId)
                 .GreaterThan(-1).WithMessage(x => InvalidProperty(nameof(x.TriggerId)));
